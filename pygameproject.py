461b01aeae88692eec8636b6dd3cf6ed8decf667
bradnielsen2981 <38126504+bradnielsen2981@users.noreply.github.com>
2021-02-09 13:41:54 +1000

    updated

--- pygameproject.py
+++ pygameproject.py
@@ -7,10 +7,9 @@ pygame.mixer.init()
 pygame.font.init()
 
 #create display
-dimensions = [1024, 600]
+dimensions = (1024, 600)
 pygame.display.set_mode(dimensions)
 pygame.display.set_caption('My Space Game')
-
 screen = pygame.display.get_surface() # Surface object
 screen.fill((124, 255, 0)) #(255,255,255)  R,G,B
 
@@ -25,34 +24,48 @@ screen.blit(label,(50,50))
 
 #create a shape or line on the screen
 #challenge: Draw three shapes or lines - look up pygame.draw
-pygame.draw.rect(screen,(255,0,0),(200,150,100,50))
 
 #play music
 pygame.mixer.music.load('backgroundmusic.mp3')
 pygame.mixer.music.play()
 
-pygame.display.flip()
 
+clock = pygame.time.Clock()
 Exit = False
-#clock = pygame.time.Clock()
-while not Exit:
-    #Process events
-    # Control the rate at which game run --> framerate set to 60fps #
-    #clock.tick(60)
+x = 0
+y = 200
+while not Exit: #game loop
 
-    ### Process Main Events and Logic ###
+    clock.tick(60) #framerate
+    # Process Main Events and Logic
     for event in pygame.event.get():
         if event.type == QUIT:
             Exit = True
+
+    #Logic
+    x = x + 1
+
+    #Drawing
+    screen.blit(background, (0,0))
+    pygame.draw.rect(screen,(255,0,0),(x,y,x+100,y+50))
+
+    #flip
+    pygame.display.flip()
+
+pygame.quit()
+sys.exit()
+
+
+    #Process events
+    # Control the rate at which game run --> framerate set to 60fps #
+
+
+   
     
     #Update game objects and do logic
     #Draw game objects
     #get_fps(), pygame.time.get_ticks() #time in milliseconds since pygame.init() was called
     #Flip screen
-
-pygame.quit()
-sys.exit(0)
-
 
 
 
